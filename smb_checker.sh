1b0351634ecb14aca384d34a247d264b9121418d
Moe <57568144+H4shByte@users.noreply.github.com>
2023-06-01 07:25:11 -0400

    Create smb_checker.sh
    Check for common smb misconfigurations

---
+++ smb_checker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Function to check for misconfigurations
+check_misconfigurations() {
+    echo "Testing SMB misconfigurations for IP: $1"
+    
+    # Check if NULL session is allowed
+    smbclient -L //$1 -N -U "%" 2>/dev/null >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "NULL Session Allowed: Yes"
+    else
+        echo "NULL Session Allowed: No"
+    fi
+
+    # Check if guest account is enabled
+    smbclient -L //$1 -N -U "guest" 2>/dev/null >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "Guest Account Enabled: Yes"
+    else
+        echo "Guest Account Enabled: No"
+    fi
+
+    # Check if anonymous login is allowed
+    smbclient -L //$1 -N 2>/dev/null >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "Anonymous Login Allowed: Yes"
+    else
+        echo "Anonymous Login Allowed: No"
+    fi
+
+    # Check if password policy is enforced
+    smbclient -L //$1 -U "test"%"password" 2>/dev/null >/dev/null
+    if [ $? -eq 0 ]; then
+        echo "Password Policy Enforced: No"
+    else
+        echo "Password Policy Enforced: Yes"
+    fi
+
+    echo "------------------------"
+}
+
+# Check if an IP address is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Please provide an IP address as an argument."
+    exit 1
+fi
+
+# Call the function to check misconfigurations for the provided IP address
+check_misconfigurations "$1"

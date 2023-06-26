408bb673825f452415e37c10febdbc5817adb923
Moe <57568144+PoSH-Father@users.noreply.github.com>
2023-06-26 17:53:30 -0400

    Update ObjectACL_Enum.ps1

--- ObjectACL_Enum.ps1
+++ ObjectACL_Enum.ps1
@@ -1,21 +1,20 @@
-# Leverage information from Get-ObjectACL from the PowerView.ps1 script (from PowerSploit)
-# Run the command "Get-Objectacl -identity <object-name>" and store it in a variable called $ace
-# Then run this script with ". .\ObjectACL_Enum.ps1" to get a $results variable that maps all ObjectSIDs, its AccessRightsProperties to the Security_identifier
-# Example: do a get-object ACL on a Domain Group -> feed that into this script to find out objects have rights over that group.
-# Interesting Rights that may be abused -> GenericAll, GenericWrite, WriteOwner, WriteDACL, AllExtendedRights, ForceChangePassword, Self
+$groups = get-netgroup | select-object -expandproperty samaccountname
 
-
-$results = foreach($Val in $ace){
+$results = foreach($group in $groups) {
+    $ace = get-objectacl -identity $group
+        foreach($Val in $ace){
 
 #Security Identifer has these rights on the Object SID
-
-    $Obj_SID = Convert-SidToName $val.ObjectSID
-    $Rights  = $val.ActiveDirectoryRights
-    $Sec_ID  = Convert-SidToName $val.SecurityIdentifier
+        $Obj_SID = Convert-SidToName $val.ObjectSID
+        $Rights  = $val.ActiveDirectoryRights
+        $Sec_ID  = Convert-SidToName $val.SecurityIdentifier
     
-    [PSCustomObject]@{
-        Object_SID      = $Obj_SID
-        Rights          = $Rights
-        Sec_Identifier  = $Sec_ID
+        [PSCustomObject]@{
+            Object_SID      = $Obj_SID
+            Rights          = $Rights
+            Sec_Identifier  = $Sec_ID
+        }
     }
 }
+
+$results | export-csv C:\Users\web_svc\Documents\test.csv
